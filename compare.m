0a76391ed999bbf41760326577fae273ea2ba787
yy550956983 <550956983@qq.com>
2016-12-16 00:34:20 +0800

    exciter test
    exciter simulink model

--- compare.m
+++ compare.m
@@ -1,4 +1,5 @@
 function compare(a)
-out=importdata('C:\\Users\\Administrator.WIN7U-20131225W\\Desktop\\out.txt');
+% out=importdata('C:\\Users\\Administrator.WIN7U-20131225W\\Desktop\\out.txt');
+out=importdata('C:\\Users\\Administrator\\Desktop\\out.txt');
 plot(a(:,1),a(:,2),'red -',out(:,1),out(:,2),'blue --');
 end
