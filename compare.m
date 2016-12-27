54ce77f6e35a7978ab63f053133a832e040a6fb4
yy550956983 <550956983@qq.com>
2016-12-27 14:06:08 +0800

    fix

--- compare.m
+++ compare.m
@@ -1,5 +1,4 @@
 function compare(a)
-% out=importdata('C:\\Users\\Administrator.WIN7U-20131225W\\Desktop\\out.txt');
-out=importdata('C:\\Users\\Administrator\\Desktop\\out.txt');
+out=importdata('C:\\out.txt');
 plot(a(:,1),a(:,2),'red -',out(:,1),out(:,2),'blue --');
 end
